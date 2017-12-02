5e4a5ec0c8825d70f65bf5668bace32c98a675fb
Bilal Karim <beekay-@users.noreply.github.com>
2017-12-02 12:26:51 -0700

    Added getData route to handle loading of markers

--- app/views.py
+++ app/views.py
@@ -8,9 +8,27 @@ from bson.objectid import ObjectId
 import time
 import random
 import os
-
 from moves_utilities import *
+
+client = MongoClient()
+visualog = client['visualog']
 
 @app.route("/")
 def index():
+    return redirect(url_for('gmap'))
+
+@app.route("/map")
+def gmap():
     return render_template("gmap.html")
+
+@csrf.exempt
+@app.route('/getData', methods=['POST','GET'])
+def getData():
+	# dataType = request.args.get("type", None)
+	boundingBox = [json.loads(request.data)]
+	JSONData = []
+	# if dataType == 'places':
+	dataCollection = visualog["places"]
+	for data in dataCollection.find({"geometry":{"$geoWithin": {"$geometry": {"type" : "Polygon" ,"coordinates": boundingBox}}}},{"properties.place.name":1, "properties.category":1, "geometry":1}):
+		JSONData.append({"type":"Feature","geometry":data["geometry"], "properties": {"name":data["properties"]["place"]["name"], "category":data["properties"]["category"]}})
+	return jsonify(data = {"type":"FeatureCollection", "features":JSONData})
