6c16cbbff6ab06b3e51df4ba15a5bbeb8e8c7889
Bilal Karim <beekay-@users.noreply.github.com>
2017-12-02 12:25:00 -0700

    Added secret_key to enable CSRF protection

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,8 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 
 # CSRF PROTECT
+from keys import secret_key
+app.secret_key = secret_key
 from flask_wtf.csrf import CsrfProtect
 csrf = CsrfProtect(app)
 
